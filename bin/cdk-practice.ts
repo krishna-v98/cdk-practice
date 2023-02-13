309f28552bb45cafbc2df43c235bd629a9fe175b
Krishna Vamsee Vangipurapu <krishnavangipurapu@gmail.com>
2023-02-13 00:16:44 -0800

    Add cdk base and frontend

--- bin/cdk-practice.ts
+++ bin/cdk-practice.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkPracticeStack } from '../lib/cdk-practice-stack';
+import * as cdk from '@aws-cdk/core';
+import { FargateDemoStack } from '../lib/fargate';
 
 const app = new cdk.App();
-new CdkPracticeStack(app, 'CdkPracticeStack', {
+new FargateDemoStack(app, 'FargateDemoStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new CdkPracticeStack(app, 'CdkPracticeStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '846019682872', region: 'us-west-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
